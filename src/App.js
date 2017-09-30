15310f46ecf69dd4c55b0c0e903ca1be98120ce4
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-30 14:18:13 +0200

    :sparkles: Add actual header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            Twilio Sync Inspector
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { injectGlobal } from 'styled-components';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
